ebd0d72713091b6adc452f49395e2ff60cd5c52f
nitslaszlo <nitslaszlo@gmail.com>
2023-09-24 21:55:11 +0200

    first commit

---
+++ src/pages/EmptyPage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+// import { useStore } from "src/stores/store";
+// import { onMounted } from "vue";
+// import { date } from "quasar";
+// import router from "src/router";
+
+// const store = useStore();
+
+// onMounted(() => {
+//   store.many_GetAll();
+// });
+</script>
+
+<template>
+  <q-page class="q-pa-xl">
+    <h1>árvíztűrőtükörfúrógép</h1>
+    <h1>ÁRVÍZTŰRŐTÜKÖRFÚRÓGÉP</h1>
+  </q-page>
+</template>
+
+<style lang="scss" scoped></style>
